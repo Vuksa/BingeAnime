68f5488fa020678d390b7b780298abc8ded7d056
Nebojsa Vuksic <nebojsa92vuksic@gmail.com>
2021-11-20 12:47:09 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Binge-Anime
+The app that allows you to track anime episodes that you watched.
---
+++ app/src/main/java/com/techwolf/android/bingeanime/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.techwolf.android.bingeanime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
